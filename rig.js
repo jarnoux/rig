7ef9035fb28f9d67508d0b4165c7c863ab6acf59
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-06-19 18:34:47 -0700

    added gitignore and fixed static middleware config

--- rig.js
+++ rig.js
@@ -1,20 +1,29 @@
+/*jslint nomen: true */
+
 var path     = require('path'),
     Config   = require('./lib/config'),
     Router   = require('./lib/router'),
     Registry = require('./middleware/registry'),
     express  = require('express'),
     Rig = function (options) {
-        var that = this;
+        'use strict';
+        var that = this,
+            staticConfig;
 
         this.app = express();
 
         // register all the middlewares
         this.registry = new Registry(new Config(options.config));
         this.registry.register(path.resolve(__dirname, 'middleware'));
+
+        staticConfig = this.registry.getConfig('middleware.static');
+        if (staticConfig) {
+            staticConfig = path.resolve(process.cwd(), staticConfig);
+        }
         this.registry.register({
             'middleware.router'      : function () {return that.app.router; },
             // make static root understand relative path
-            'middleware.static'      : express.static.bind(null, path.resolve(process.cwd(), this.registry.getConfig('middleware.static'))),
+            'middleware.static'      : express.static.bind(null, staticConfig),
             'middleware.logger'      : express.logger,
             'middleware.query'       : express.query,
             'middleware.bodyParser'  : express.bodyParser,
@@ -34,10 +43,12 @@ var path     = require('path'),
     };
 
 Rig.prototype.register = function (name, resource) {
+    'use strict';
     this.registry.register(name, resource);
 };
 
 Rig.prototype.map = function () {
+    'use strict';
     this.router.map(this.app);
 };
 
