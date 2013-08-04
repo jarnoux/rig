ed039404ed2e45a6f26c1ee375a81bdeb13b3f47
gomezd <david.garcia.mx@gmail.com>
2013-08-03 18:25:10 -0700

    fixed json error handler

--- middleware/tojson-errorhandler.js
+++ middleware/tojson-errorhandler.js
@@ -5,13 +5,10 @@ module.exports = function (options) {
         var type = err.name || err.constructor.name,
             status = options.statuses[type] || 500;
 
-        res.writeHead(status);
-        req.renderJSON({
+        res.json(status, {
             type: type,
             status: status,
             message: err.message
-        }, function (err, string) {
-            res.end(err || string);
         });
     };
 };
