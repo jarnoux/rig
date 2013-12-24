d87a352a1b574f4ad0a567ab42b4778619674bef
Jacques Arnoux <arnoux.jacques@gmail.com>
2013-12-24 19:08:40 +0100

    smallfix

--- middleware/request-id.js
+++ middleware/request-id.js
@@ -1,7 +1,8 @@
 
 module.exports = function (options) {
+    'use strict';
 	var counter = 0;
-	return function(req, res, next) {
+	return function (req, res, next) {
 		req.id = counter++;
 		next();
 	}
